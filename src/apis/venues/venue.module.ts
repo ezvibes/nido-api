c048bd898f98efe92d10aeca86f124a284c7be3b
Evan Bonertz <ezvibes@Evans-MacBook-Pro.local>
2026-07-08 19:47:48 -0400

    fix: import AuthModule in VenueModule to resolve FirebaseAuthGuard dependencies

--- src/apis/venues/venue.module.ts
+++ src/apis/venues/venue.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Venue } from './entities/venue.entity';
 import { VenueService } from './venue.service';
 import { VenueController } from './venue.controller';
+import { AuthModule } from '../../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Venue])],
+  imports: [
+    TypeOrmModule.forFeature([Venue]),
+    AuthModule,
+  ],
   controllers: [VenueController],
   providers: [VenueService],
   exports: [VenueService],
